272ed3bdda4b0c6b07ccc08687e7b57086a3a0e0
astrocaribe <tlcommodore@gmail.com>
2012-08-29 17:14:49 -0400

    Added commando line arguments
    Added commando line arguments to accomplish the following:
    i)   -h --help to offer usage help
    ii)  -f --file to allows user to change the input file

--- MyFirstScript.py
+++ MyFirstScript.py
@@ -7,8 +7,10 @@ __version__ = 0.2
 
 import numpy as np
 import pylab as pl
+import argparse as ap
 
 def mkplot(outfile,xx,yy1,yy2,yy3,yy4,ylab='Slope Unc. [e-/s]'):
+
 	pl.loglog(xx, yy1, 'b--', xx, yy2, 'r:', xx, yy3, 'g-', xx, \
 		yy4, 'm-.', linewidth=3)
 	pl.legend(('Random Uncertainty','Correlated Uncertainty', \
@@ -21,10 +23,16 @@ def mkplot(outfile,xx,yy1,yy2,yy3,yy4,ylab='Slope Unc. [e-/s]'):
 	return
 
 if __name__=='__main__':
-	infile = 'Gordon2005_Fig16.txt'
+
+	parser = ap.ArgumentParser(description='Make a plot.')
+	parser.add_argument('-f', '--file', default='Gordon2005_Fig16.txt', \
+	  type=str, help='Input file.')
+	options = parser.parse_args()
+ 	
+	infile = options.file
 	slope_outfile = 'fig16_slope.pdf'
 	yint_outfile = 'fig16_yint.pdf'
-	
+		
 	slope, ran_slope_unc, corr_slope_unc, both_slope_unc, \
 	  eqn_slope_unc, ran_yint_unc, corr_yint_unc, both_yint_unc, \
 	  eqn_yint_unc = np.loadtxt(infile, unpack=True)
