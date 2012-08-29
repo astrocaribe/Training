e6bebbc9c0a66bdc202b9cc6c25ca5e091a7d83b
astrocaribe <tlcommodore@gmail.com>
2012-08-29 15:39:30 -0400

    Added functionality to initial script
    . Added function mkplot to plot data read in from input file
    . Added the ability to plot both the slope and y-intercept uncertainty
    (separate plots)

--- MyFirstScript.py
+++ MyFirstScript.py
@@ -1,38 +1,35 @@
 #! /usr/bin/env python
 
-'''
-ABOUT:
-This is a program to read in the Gordon et  al. 2005
-Figure 16 information in order to recreate the plot.
+#Header
 
-DEPENDS:
-Python 2.7.1
-
-AUTHOR:
-T.S. Le Blanc for STScI, 2012
-
-HISTORY:
-2012, Aug:	Initial script
-
-USE:
-python MyFirstScript.py
-'''
+__author__ = 'T.S. Le Blanc'
+__version__ = 0.2
 
 import numpy as np
 import pylab as pl
 
-infile = 'Gordon2005_Fig16.txt'
-outfile = 'fig16_log.pdf'
+def mkplot(outfile,xx,yy1,yy2,yy3,yy4,ylab='Slope Unc. [e-/s]'):
+	pl.loglog(xx, yy1, 'b--', xx, yy2, 'r:', xx, yy3, 'g-', xx, \
+		yy4, 'm-.', linewidth=3)
+	pl.legend(('Random Uncertainty','Correlated Uncertainty', \
+		'Both','Equation'), 'best')
+	pl.xlabel('Slope [e-/s]')
+	pl.ylabel(ylab)
+	pl.savefig(outfile)
+	pl.clf()
+	print 'File saved to: ', outfile
+	return
 
-slope, ran_slope_unc, corr_slope_unc, both_slope_unc, \
-	eqn_slope_unc = np.loadtxt(infile, 
-	usecols = (0,1,2,3,4), unpack=True)
-
-pl.loglog(slope, ran_slope_unc, 'b--', slope, corr_slope_unc, 'r:', \
-	slope, both_slope_unc, 'g-', slope, eqn_slope_unc, 'm-.', linewidth=2)
-pl.legend(('Random Uncertainty','Correlated Uncertainty','Both','Equation'), \
-	'best')
-pl.xlabel('Slope [e-/s]')
-pl.ylabel('Slope Unc. [e-/s]')
-pl.savefig('fig16_log.pdf')
-pl.clf()
+if __name__=='__main__':
+	infile = 'Gordon2005_Fig16.txt'
+	slope_outfile = 'fig16_slope.pdf'
+	yint_outfile = 'fig16_yint.pdf'
+	
+	slope, ran_slope_unc, corr_slope_unc, both_slope_unc, \
+	  eqn_slope_unc, ran_yint_unc, corr_yint_unc, both_yint_unc, \
+	  eqn_yint_unc = np.loadtxt(infile, unpack=True)
+		
+	mkplot(slope_outfile, slope, ran_slope_unc, corr_slope_unc, \
+	  both_slope_unc, eqn_slope_unc)
+	mkplot(yint_outfile, slope, ran_yint_unc, corr_yint_unc, \
+	  both_yint_unc, eqn_yint_unc, ylab='Y-Intercept Unc. [e-]')
