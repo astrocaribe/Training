f2acf93332139b7865013dd1694931688e823c35
astrocaribe <tlcommodore@gmail.com>
2012-09-18 16:50:02 -0400

    Minor script changes
    - Included an "if __name__ == '__main__': " control block. Pervents
    code from running if importing w/ "import dao_tasks", for example.

--- Photometry/dao_tasks.py
+++ Photometry/dao_tasks.py
@@ -19,28 +19,30 @@ import pyraf, os, glob
 from pyraf import iraf
 from iraf import noao, digiphot, daophot
 
-#generate list of all fits files in current directory
-file_list = glob.glob('*_cts.fits')
-print file_list
+if __name__ == '__main__':
 
-#loops through all FITS files
-for file in file_list:
-    #look for old files, and remove if they exist
-    file_query = os.access('starlist_' + file.replace('.fits', ''), \
-      os.R_OK)
-    if file_query == True:
-        os.remove('starlist_' + file.replace('.fits', ''))
+    #generate list of all fits files in current directory
+    file_list = glob.glob('*_cts.fits')
+    print file_list
+
+    #loops through all FITS files
+    for file in file_list:
+        #look for old files, and remove if they exist
+        file_query = os.access('starlist_' + file.replace('.fits', \
+        ''), os.R_OK)
+        if file_query == True:
+            os.remove('starlist_' + file.replace('.fits', ''))
     
-    #execute daofind on one image
-    print 'DAOFind on image ', file
-    outfile = 'starlist_' + file.replace('.fits', '')
-    iraf.daofind.unlearn()
-    iraf.daofind(
-        image = file,
-        output = outfile,
-        fwhmpsf = 3,
-        readnoise = 7.6424,
-        itime = 7024.,
-        threshold = 4.0,
-        interactive = 'no',
-        verify = 'no') 
+        #execute daofind on one image
+        print 'DAOFind on image ', file
+        outfile = 'starlist_' + file.replace('.fits', '')
+        iraf.daofind.unlearn()
+        iraf.daofind(
+            image = file,
+            output = outfile,
+            fwhmpsf = 3,
+            readnoise = 7.6424,
+            itime = 7024.,
+            threshold = 4.0,
+            interactive = 'no',
+            verify = 'no') 
